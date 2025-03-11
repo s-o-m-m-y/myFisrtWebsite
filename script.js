21056196300913560ccff6d88b0b2052ec688e15
s-o-m-m-y <123276217+s-o-m-m-y@users.noreply.github.com>
2025-03-10 21:41:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+document.addEventListener("DOMContentLoaded"
+, function () {
 const btn = 
-document.getElementById('btn');
-
-const navLinks =
-document.querySelector('.menu');
-
-
-btn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    document.getElementById("btn");
+    const navLinks =
+        document.querySelector(".menu");
+    btn.addEventListner("click", function() {
+        console.log("Menu button clicked!");
+        navLinks.classList.toggle("active");
+    });
 });
-```
-
+        
